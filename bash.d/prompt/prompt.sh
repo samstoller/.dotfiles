066a4c5fe54c466b20fc1829578c29f89bd98e4e
Sam Stoller <snstoller@gmail.com>
2014-10-15 23:44:58 -0700

    Update prompt with a more modern look

--- bash.d/prompt/prompt.sh
+++ bash.d/prompt/prompt.sh
@@ -7,4 +7,8 @@ if [ -n "$INSTANCE_ID" ]; then
     MYHOST="$INSTANCE_ID"
 fi
 
-PS1="\[$PS_Green\]<\[$PS_BGreen\]\u\[$PS_Green\]@$MYHOST> \[$PS_BBlue\]\w\$(__git_ps1 \"\[$PS_Grey\]:\[$PS_Red\]%s\") \[$PS_Grey\]\$\[$PS_NC\] "
+#PS1="\n\[$PS_Yellow\][\u] \[$PS_Grey\][$MYHOST] \[$PS_Green\][\w]\n\[$PS_Blue\][\W]\$(__git_ps1 \"\[$PS_Red\] [%s]\") \[$PS_Grey\]\$\[$PS_NC\] "
+
+PS1="\[$PS_White\]\w\n\[$PS_Yellow\] \u \[$PS_Grey\][$MYHOST] \$(__git_ps1 \"\$PS_BRed\]%s\[$PS_Grey\]:\")\[$PS_Cyan\]\W \[$PS_Grey\]\$\[$PS_NC\] "
+#PS1="\n\$(__git_ps1 \"\[$PS_Red\][%s] \")\[$PS_Green\][\w]\n\[$PS_Yellow\][\u] \[$PS_Grey\][$MYHOST] \[$PS_Grey\]\$\[$PS_NC\] "
+#PS1="\[$PS_Green\]<\[$PS_BGreen\]\u\[$PS_Green\]@$MYHOST> \[$PS_BBlue\]\w\$(__git_ps1 \"\[$PS_Grey\]:\[$PS_Red\]%s\") \[$PS_Grey\]\$\[$PS_NC\] "
