79da3af133f5a05e74c995f4b95e43f577c73e32
Sam Stoller <snstoller@gmail.com>
2014-11-02 23:00:34 +0000

    Updated twitter_theme Irssi script to v0.8a

--- irssi/scripts/twitter_theme.pl
+++ irssi/scripts/twitter_theme.pl
@@ -1,22 +1,11 @@
 #!/usr/bin/perl -w
 #
-# twittertheme.pl
+# Twitter Theme (twitter_theme.pl)
 #
-# Colorizes Twitter message components (configured for Bitlbee-style).
-# Removes redundant long-format URLs.
+# Colorizes Twitter-like message components (configured for Bitlbee-style).
+# Removes redundant URLs.
 #
-# Note: Will remove existing color formatting on message part.
-#
-# Can be configured to colorize the following components:
-# * [alphanum]
-# * RT
-# * @usertag
-# * #hashtag
-# * http(s)://url
-# * 'plain text'
-#
-# Can be configured to remove URLs that:
-# * Appear inside brackets <...>
+# Note: Will remove existing color and formatting on message part.
 #
 # CHANGELOG
 #
@@ -59,7 +48,7 @@
 
 use strict;
 use warnings;
-use vars qw($VERSION %IRSSI %COLORS);
+use vars qw($VERSION %IRSSI %COLORS %COMPONENTS);
 use Irssi;
 
 #use Data::Dumper;
@@ -74,8 +63,8 @@ $VERSION = "0.4";
     name        => "Twitter Theme",
     description => "Assign colors to tweet message components",
     license     => "Public Domain",
-    url         => "http://irssi.org/",
-    changed     => "2014-03-01"
+    url         => "http://github.com/samstoller/irssi-twitter-theme",
+    changed     => "2014-10-30"
 );
 %COLORS = (
     white    => 0,
@@ -95,6 +84,14 @@ $VERSION = "0.4";
     gray     => 14,
     lgray    => 15
 );
+%COMPONENTS = (
+    bitlbee  => 'lgray',
+    hash     => 'yellow',
+    http     => 'gray',
+    retweet  => 'black green',
+    text     => 'white',
+    user     => 'magenta'
+);
 
 #######################
 # Command subroutines #
@@ -103,8 +100,9 @@ $VERSION = "0.4";
 sub cmd_twt {
     if ( $_[0] eq 'colors' ) {
         cmd_colors();
-    }
-    else {
+    } elsif ( $_[0] eq 'reset' ) {
+        cmd_reset();
+    } else {
         cmd_help();
     }
 }
@@ -112,89 +110,70 @@ sub cmd_twt {
 sub cmd_colors {
     Irssi::print(
         <<EOF
-
 Colors:
 
-  Below is a list of colors that you can set your components to. The prefix of 'l' 
-  indicates a lighter version which is often an unbolded version of the base color.
+  Below is a list of colors that you can set your components to. The prefix of 'l'
+  indicates a lighter version which is often a bolded version of the base color.
 
-  Please note that your terminal may display these colors differently than described.
+  Only certain colors are available as background colors as shown below.
 
-    white
-    black
-    gray     lgray
-    red      lred
-    green    lgreen
-    blue     lblue
-    magenta  lmagenta
-    cyan     lcyan
-    yellow   lyellow
-
-Examples:
-
-  List all Twitter Theme settings:
-    /SET twt_
-
-  Set the colorization theme for URLs:
-    /SET twt_color_http gray
-
-  Set the colorization theme for hashtags:
-    /SET twt_color_hash yellow lblue
-
+  Note: your terminal may display these colors differently than described.
 EOF
         , MSGLEVEL_CLIENTCRAP
     );
+
+    print_colors();
 }
 
 sub cmd_help {
+    Irssi::print( chr(3).sprintf( '%02d', $COLORS{'white'} ).','.sprintf( '%02d', $COLORS{'blue'}).'Twitter Theme', MSGLEVEL_CLIENTCRAP );
     Irssi::print(
         <<EOF
 
 Description:
 
-  Twitter Theme colorizes Twitter-like components in your public messages (channels). Twitter Theme 
-  is configured for a Bitlbee-style tweets but should work with most other Irssi Twitter clients 
-  or you can just enable it for normal IRC channels!
+  Twitter Theme colorizes Twitter-like components in your public messages (channels). Twitter Theme
+  is configured for a Bitlbee-style tweets but should work with other Irssi Twitter clients.
+  You can also use this script for regular IRC channels.
 
-Known Issues:
-  
-  * Existing colors and formatting will be removed from the message part. Note that the message 
-    does not include nicks, so nick color will be preserved, however highlights will not.
+Usage:
 
-  * Channels of the same name across different servers cannot be individually configured. For 
-    example, setting your channel list to #twitter is server-agnostic and will colorize all 
-    #twitter channels regardless of which server you are connected to.
-
-Usage/Options:
-
-  Twitter Theme works out of the box and does not need to be configured for first time use. 
-  However, you will most likely want to restrict the script to specific channels and set your 
-  colors. All configuration is done through Irssi settings via the /SET or /TOGGLE commands.
+  Twitter Theme works out of the box and does not need to be configured for first time use.
+  However, you will most likely want to restrict the script to specific channels and define your
+  own color schemes for each component. All configuration is done through Irssi settings via
+  the /SET or /TOGGLE commands.
 
 Channel Config (default is all):
 
   Channel names must start with a #.
 
   /SET twt_channels [<chan1> <chan2> <chan3> ...] or [all]
-    
+
 Color Config:
-    
-  With Twitter Theme, you can colorize up to six different message components as described below. 
-  In the very least you must set the foreground aka text color, the background color is optional. 
+
+  With Twitter Theme, you can colorize up to six different message components as described below.
+  In the very least you must set the foreground (text) color, the background color is optional.
   For a list of valid colors, see the 'Colors' section below.
 
   Setting Name           Foreground  Background   Component
 
-  /SET twt_color_bitlbee  [<color>]  [<color>]    [0x->0x]   - Bitlbee tweet numbers
-  /SET twt_color_hash     [<color>]  [<color>]    #hashtags  - Twitter hashtags
-  /SET twt_color_http     [<color>]  [<color>]    http://    - Basic URLs
-  /SET twt_color_retweet  [<color>]  [<color>]    RT         - Bitlbee retweets
-  /SET twt_color_text     [<color>]  [<color>]    'string'   - All text
-  /SET twt_color_user     [<color>]  [<color>]    \@usertags  - Twitter usernames
+  /SET twt_color_bitlbee  [<color>]  [<color>]    [0x->0x]    - Bitlbee IDs
+  /SET twt_color_hash     [<color>]  [<color>]    #hashtags   - Hashtags
+  /SET twt_color_http     [<color>]  [<color>]    http(s)://  - URLs
+  /SET twt_color_retweet  [<color>]  [<color>]    RT          - Retweets
+  /SET twt_color_text     [<color>]  [<color>]    'string'    - Text
+  /SET twt_color_user     [<color>]  [<color>]    \@usertags  - Usernames
+EOF
+        , MSGLEVEL_CLIENTCRAP 
+    );
 
+    cmd_colors();
+
+    Irssi::print(
+    <<EOF
 Removing Long URLs:
-    
-  You can also toggle the removal of long URLs which are defined as any text that looks like a URL 
+
+  You can also toggle the removal of long URLs which are defined as any text that looks like a URL
   that is between two angle bracket characters such as:
 
       <https://myurl.com>
@@ -202,40 +181,55 @@ Removing Long URLs:
 
     /SET twt_remove_long_urls  [ON|OFF|TOGGLE]
 
-Colors:
+Known Issues:
 
-  Below is a list of colors that you can set your components to. The prefix of 'l' 
-  indicates a lighter version which is often an unbolded version of the base color.
+  * Existing colors and formatting will be removed from the message part. Note that the message
+    does not include nicks, so nick color will be preserved, however highlights will not.
 
-  Please note that your terminal may display these colors differently than described.
-
-    white
-    black
-    gray     lgray
-    red      lred
-    green    lgreen
-    blue     lblue
-    magenta  lmagenta
-    cyan     lcyan
-    yellow   lyellow
+  * Channels of the same name across different servers cannot be individually configured. For
+    example, setting your channel list to #twitter is server-agnostic and will colorize all
+    #twitter channels regardless of which server you are connected to.
 
 Examples:
 
-  List all Twitter Theme settings:
-    /SET twt_
+  /SET twt_
+        List all Twitter Theme settings
 
-  Set list of channels to three specific channels:
-    /SET twt_channels #perl #irssi #twitter
+  /SET twt_channels #perl #irssi #twitter
+        Set list of channels to three specific channels
 
-  Set the colorization theme for URLs:
-    /SET twt_color_http gray
+  /SET twt_color_http gray
+        Set the colorization theme for URLs
 
-  Set the colorization theme for hashtags:
-    /SET twt_color_hash yellow lblue
+  /SET twt_color_hash yellow lblue
+        Set the colorization theme for hashtags
+
+Commands:
+
+  /twt
+        This help text
+
+  /twt colors
+        Display available colors
+
+  /twt reset
+        Reset colors to the default theme
 
 EOF
         , MSGLEVEL_CLIENTCRAP
     );
+}
+
+sub cmd_reset {
+
+    # Component defaults are in the global space
+    while( my($k, $v) = each %COMPONENTS ) {
+        Irssi::settings_set_str( 'twt_color_' . $k, $v );
+    }
+    Irssi::settings_set_str( 'twt_channels', 'all' );
+    Irssi::settings_set_bool( 'twt_remove_long_urls', 1 );
+
+    Irssi::print('Twitter Theme settings reset.');
 }
 
 ######################
@@ -273,6 +267,7 @@ sub sig_setup_changed {
 sub colorize {
     my ( $msg, $target ) = @_;
     my $pretty_msg = '';
+    my $previous = '';
 
     # Is this channel set to be colorized?
     return $msg if ( !is_enabled_chan($target) );
@@ -287,15 +282,25 @@ sub colorize {
         # Skip long URLs if setting enabled
         if ( not( has_remove_long_URLs() and is_long_URL($word) ) ) {
 
-            # Color code
-            $pretty_msg .= get_component_color( detect_component($word) );
+            # Get the type of component and if it's the first component
+            my $component = detect_component($word);
+            my $msg_start = $pretty_msg eq '' ? 1 : 0;
 
-            # Component
-            $pretty_msg .= $word . chr(15) . ' ';    # TODO/FIXME whitespace
+            # Modify word hook
+            $word = modify_word($word, $component);
+
+            # If this is part of the text message then don't reset colors
+            if ($component eq 'text' && $component eq $previous) {
+                $pretty_msg .= ($msg_start ? '' : ' ') . $word;
+            } else {
+                $pretty_msg .= chr(15) . ($msg_start ? '' : ' ') . get_component_color($component) . $word;
+            }
+
+            $previous = $component;
         }
     }
 
-    return $pretty_msg;
+    return $pretty_msg . chr(15);
 }
 
 sub detect_component {
@@ -319,11 +324,22 @@ sub detect_component {
         $component = 'hash';
 
     }
-    elsif ( $word =~ /^https?:\/\// ) {
+    elsif ( $word =~ /^\(?https?:\/\// ) {
         $component = 'http';
     }
 
     return $component;
+}
+
+sub modify_word {
+    my ($word, $component) = @_;
+
+    # Remove leading open parens from URLs
+    if ( $component eq 'http' ) {
+        $word =~ s/^\(?(.*)$/$1/;
+    }
+
+    return $word;
 }
 
 sub is_long_URL {
@@ -407,7 +423,7 @@ sub validate_channels {
 }
 
 sub validate_colors {
-    foreach my $component (qw(bitlbee hash http retweet text user)) {
+    foreach my $component (keys %COMPONENTS) {
 
         my $setting = '';
         my $to_validate
@@ -432,12 +448,47 @@ sub validate_colors {
     }
 }
 
+sub print_colors {
+    Irssi::print(
+        "Foreground:\n".
+        chr(3).sprintf( '%02d', $COLORS{'white'} ).','.sprintf( '%02d', $COLORS{'black'} ) . '  white      ' .
+        chr(3).sprintf( '%02d', $COLORS{'black'} ).','.sprintf( '%02d', $COLORS{'lgray'} ) . 'black' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'gray'}) . '  gray       ' .
+        chr(3).sprintf( '%02d', $COLORS{'lgray'}) . 'lgray' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'yellow'}) . '  yellow     ' .
+        chr(3).sprintf( '%02d', $COLORS{'lyellow'}) . 'lyellow' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'green'}) . '  green      ' .
+        chr(3).sprintf( '%02d', $COLORS{'lgreen'}) . 'lgreen' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'cyan'}) . '  cyan       ' .
+        chr(3).sprintf( '%02d', $COLORS{'lcyan'}) . 'lcyan' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'blue'}) . '  blue       ' .
+        chr(3).sprintf( '%02d', $COLORS{'lblue'}) . 'lblue' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'magenta'}) . '  magenta    ' .
+        chr(3).sprintf( '%02d', $COLORS{'lmagenta'}) . 'lmagenta' . "\n" .
+        chr(3).sprintf( '%02d', $COLORS{'red'}) . '  red        ' .
+        chr(3).sprintf( '%02d', $COLORS{'lred'}) . 'lred' . "\n",
+        MSGLEVEL_CLIENTCRAP
+    );
+    Irssi::print(
+        "Background:\n".
+        '  '.chr(3).sprintf( '%02d', $COLORS{'black'} ).','.sprintf( '%02d', $COLORS{'lgray'} ) . 'lgray' . "\n" .
+        '  '.chr(3).sprintf( '%02d', $COLORS{'black'} ).','.sprintf( '%02d', $COLORS{'yellow'} ) . 'yellow' . "\n" .
+        '  '.chr(3).sprintf( '%02d', $COLORS{'black'} ).','.sprintf( '%02d', $COLORS{'green'} ) . 'green' . "\n" .
+        '  '.chr(3).sprintf( '%02d', $COLORS{'black'} ).','.sprintf( '%02d', $COLORS{'cyan'} ) . 'cyan' . "\n" .
+        '  '.chr(3).sprintf( '%02d', $COLORS{'white'} ).','.sprintf( '%02d', $COLORS{'blue'} ) . 'blue' . "\n" .
+        '  '.chr(3).sprintf( '%02d', $COLORS{'white'} ).','.sprintf( '%02d', $COLORS{'magenta'} ) . 'magenta' . "\n" .
+        '  '.chr(3).sprintf( '%02d', $COLORS{'white'} ).','.sprintf( '%02d', $COLORS{'red'} ) . 'red' . "\n",
+        MSGLEVEL_CLIENTCRAP
+    );
+}
+
 ################
 # Main routine #
 ################
 
 # Bind (to commands)
 Irssi::command_bind( 'twt', \&cmd_twt, 'Twitter Theme' );
+Irssi::command_bind( 'twt reset', \&cmd_twt );
 Irssi::command_bind( 'twt colors', \&cmd_twt );
 
 # Bind (to signals)
@@ -445,12 +496,9 @@ Irssi::signal_add_last( 'message public',     'sig_public' );
 Irssi::signal_add_last( 'message own_public', 'sig_own_public' );
 Irssi::signal_add_last( 'setup changed',      'sig_setup_changed' );
 
-# Settings w/ defaults (/SET)
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_channels',      'all' );
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_bitlbee', 'lgray' );
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_hash',    'yellow' );
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_http',    'gray' );
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_retweet', 'green' );
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_text',    'white' );
-Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_user',    'magenta' );
-Irssi::settings_add_bool( $IRSSI{'name'}, 'twt_remove_long_urls', 1 );    # ON
+# Settings (init w/ defaults)
+while( my($k, $v) = each %COMPONENTS ) {
+    Irssi::settings_add_str( $IRSSI{'name'}, 'twt_color_' . $k, $v );
+}
+Irssi::settings_add_str( $IRSSI{'name'}, 'twt_channels', 'all' );
+Irssi::settings_add_bool( $IRSSI{'name'}, 'twt_remove_long_urls', 1 );
