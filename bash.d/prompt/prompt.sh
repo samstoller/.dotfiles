e907421319d495dbfb976b30c950e281f685f2ee
Sam Stoller <snstoller@gmail.com>
2015-03-07 22:49:07 -0800

    Prompt: Fix line overwrite issue
    * Add missing \[ to non-printing character

--- bash.d/prompt/prompt.sh
+++ bash.d/prompt/prompt.sh
@@ -1,6 +1,6 @@
 ## Prompt Definitions
 
 
-PS1="\[$PS_White\]\w\n\[$PS_Yellow\] \u \[$PS_Grey\][$PS1HOST] \$(__git_ps1 \"\$PS_BRed\]%s\[$PS_Grey\]:\")\[$PS_Cyan\]\W \[$PS_Grey\]\$\[$PS_NC\] "
+PS1="\[$PS_White\]\w\n\[$PS_Yellow\] \u \[$PS_Grey\][$PS1HOST] \$(__git_ps1 \"\[\$PS_BRed\]%s\[$PS_Grey\]:\")\[$PS_Cyan\]\W \[$PS_Grey\]\$\[$PS_NC\] "
 #PS1="\n\$(__git_ps1 \"\[$PS_Red\][%s] \")\[$PS_Green\][\w]\n\[$PS_Yellow\][\u] \[$PS_Grey\][$PS1HOST] \[$PS_Grey\]\$\[$PS_NC\] "
 #PS1="\[$PS_Green\]<\[$PS_BGreen\]\u\[$PS_Green\]@$PS1HOST> \[$PS_BBlue\]\w\$(__git_ps1 \"\[$PS_Grey\]:\[$PS_Red\]%s\") \[$PS_Grey\]\$\[$PS_NC\] "
