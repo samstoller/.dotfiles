80ca44ca1c056195250e9b1acd225eac9608aa96
Sam Stoller <samantha@slack-corp.com>
2016-08-05 12:01:34 -0700

    Update OS X alias names for `afk` and `afkss`

--- bash.d/osx.sh
+++ bash.d/osx.sh
@@ -27,10 +27,10 @@ fi
 ## Aliases ##
 
 # Lock the screen (when going AFK)
-alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
 # Start ScreenSaver. This will lock the screen if locking is enabled.
-alias afkss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+alias afk="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
 
 # Flush Directory Service cache
 alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
